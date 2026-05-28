d3206d93f45acf35747312a739008dcabaa5b150
Harigovind R <harigovindr101@gmail.com>
2026-05-28 16:00:33 +0530

    Add data retrieval and filtering queries
    Practices using SELECT to retrieve specific columns and applies the WHERE clause with various operators including numeric comparisons, date filtering, inequality, and NULL/NOT NULL state checks.

---
+++ SELECT.sql
@@ -0,0 +1,34 @@
+/*select */
+use db;
+select * from employees;
+
+/*select first name and last name*/
+select first_name,last_name from employees;
+
+/*select emp_id is 1*/
+select * from employees
+where emp_id=1;
+
+/*first name equals kaneye*/
+select * from employees where first_name = "kanye";
+
+/*hourly pay greater than or equals 70*/
+select * from employees
+where hourly_pay>=70;
+
+/*hire date less than 2026-01-01*/
+select * from employees
+where hire_date < "2026-01-01";
+
+/*select employee doesnt equals emp_id 1*/
+select * from employees
+where emp_id !=1;
+
+/*hire date is null*/
+select * from employees
+where hire_date is null;
+
+/*select first name of the employees where hire date not null*/
+select first_name from employees
+where hire_date is not null;
+
