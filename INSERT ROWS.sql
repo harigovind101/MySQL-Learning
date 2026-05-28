d3609a5536934131254f5fb2218ffa18358d7e5b
Harigovind R <harigovindr101@gmail.com>
2026-05-28 15:50:42 +0530

    Practice full and partial row insertions
    Populates the employees table using multi-row bulk INSERT statements. Also includes an example of inserting data into specific columns only, demonstrating how omitted values default to NULL.

---
+++ INSERT ROWS.sql
@@ -0,0 +1,14 @@
+use db;
+insert into employees
+values (1,'john','cena',85.5,'2027-05-13'),
+       (2,'ryan','gosling',50.75,'2026-12-12'),
+       (3,'ana','armas',45.00,'2026-02-25'),
+       (4,'robbert','patision',75.25,'2025-10-30'),
+       (5,'kanye','west',125.10,'2020-01-17');
+select * from employees;
+
+insert into employees (emp_id,first_name,last_name)
+values (6,'sydeny','sweeney');
+select * from employees;
+
+
